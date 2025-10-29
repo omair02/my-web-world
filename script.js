43a3e3b1bc1a9b29a744918c57698fec47eff565
Muhammad Omair <omair4u2004@gmail.com>
2025-10-29 19:51:49 +0530

    Added my first web development project files

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+  alert("Hello, Muhammad Omair! 👋 Welcome to your first web development project!");
+  document.getElementById("afterClick").innerText = "🎉 You clicked the button successfully!";
+}
